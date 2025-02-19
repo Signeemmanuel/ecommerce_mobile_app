e2133766f45dbaa3ad866e34756ee8d58dc633c3
Signeemmanuel <signeemmanuel28@gmail.com>
2025-02-19 19:09:58 +0100

    Create project file structure and setup textTheme

---
+++ lib/utils/theme/custom_themes/elevated_button_theme_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TElevatedButtonThemeData {
+  TElevatedButtonThemeData._();
+
+  static ElevatedButtonThemeData lightElevatedButtonThemeData = ElevatedButtonThemeData();
+  static ElevatedButtonThemeData darkElevatedButtonThemeData = ElevatedButtonThemeData();
+}
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce_mobile_app/utils/theme/custom_themes/elevated_button_theme_data.dart';
+import 'package:ecommerce_mobile_app/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonThemeData.lightElevatedButtonThemeData,
+
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonThemeData.darkElevatedButtonThemeData,
+  );
+}
